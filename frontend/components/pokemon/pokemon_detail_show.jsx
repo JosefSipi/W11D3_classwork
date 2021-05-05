6a9b564abd61da5fc6f142681ddc68ca71a2bd0f
your name here <abirmahamud610@gmail.com>
2021-05-05 17:20:59 -0400

    Finish pokedex

---
+++ frontend/components/pokemon/pokemon_detail_show.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import configureStore from '../../store/store';
+
+
+class PokemonDetail extends React.Component{
+    constructor(props){
+        super(props)
+
+        
+    }
+
+    render(){
+        
+        // const store = configureStore();
+        return(
+
+        )
+    }
+}
