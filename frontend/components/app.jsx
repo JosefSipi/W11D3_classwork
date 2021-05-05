56652d063cf8428df674fc0dea5a0439b36f39d6
your name here <abirmahamud610@gmail.com>
2021-05-05 14:08:44 -0400

    Populate App component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,6 @@
 import { Route } from "react_router_dom";
 import PokemonIndex from "./pokemon/pokemon_index";
+import PokemonIndexContainer from "./pokemon/pokemon_index_container";
 
 const App = () => (
     <Route path="/" component={PokemonIndexContainer} />
